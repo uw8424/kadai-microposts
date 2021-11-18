1489ff07831a89866e63efe4e5c313ba4c3d123e
naoya.kato <xariarl84@gmail.com>
2021-11-18 16:35:42 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,18 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">Microposts</a>
+        
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            
+        </button>
+            
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav"></ul>
+            
+                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                
+                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+        </div>    
+    </nav>
+</header>
