84ef934fe556ecf17075b3048cb88c6c2cad93db
naoya.kato <xariarl84@gmail.com>
2021-11-22 14:45:54 +0000

    post

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::orderBy("id", "desc")->paginate(10);
+        
+        return view("users.index",[
+            "users" => $users,
+            ]);
+    }
+    
+    public function show($id)
+    {
+        // idの値でユーザーを検索して取得
+        $user = User::findOrFail($id);
+        
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+        
+        //ユーザの投稿一覧を作成日時の降順で取得
+        $microposts = $user->microposts()->orderBy("created_at", "desc")->paginate(10);
+        
+        // ユーザー詳細ビューでそれらを表示
+        return view("users.show", [
+            "user" => $user,
+            "microposts" => $microposts,
+            ]);
+    }
+}
--- app/User.php
+++ app/User.php
@@ -36,4 +36,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function microposts()
+    {
+        return $this->hasMany(Micropost::class);
+    }
+    
+    public function loadrelationshipCounts()
+    {
+        $this->loadCount("microposts");
+    }
+    
 }
